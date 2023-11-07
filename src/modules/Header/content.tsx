3801ae2f6c3c2248c741d5de16b7a61a3e9647f3
Russell Dias <russell.dias98@gmail.com>
2023-11-07 17:32:44 +0530

    added new content

---
+++ src/modules/Header/content.tsx
@@ -0,0 +1,16 @@
+import { Contact, DraftingCompass, Smile } from "lucide-react";
+
+export const headerItems = [
+  {
+    title: "About",
+    icon: <Smile className="w-4 h-4 mr-2" />,
+  },
+  {
+    title: "Projects",
+    icon: <DraftingCompass className="w-4 h-4 mr-2" />,
+  },
+  {
+    title: "Contact",
+    icon: <Contact className="w-4 h-4 mr-2" />,
+  },
+];
