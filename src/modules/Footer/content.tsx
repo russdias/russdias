bc6dffb432826366ecc766ed462b8bb16cf598f9
Russell Dias <russell.dias98@gmail.com>
2023-11-07 17:40:01 +0530

    added footer content

---
+++ src/modules/Footer/content.tsx
@@ -0,0 +1,9 @@
+export const content = {
+  header: (
+    <small>
+      Designed and Developed by Russell Dias. <br />
+      <br /> Built with Next.js, Shadcn UI, Tailwind CSS, and deployed with
+      Vercel.
+    </small>
+  ),
+};
